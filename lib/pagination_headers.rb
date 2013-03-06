0b1cedd4a442162c18f354e604d5740d09c0847a
Feras Hirzalla <hirzalla@gmail.com>
2013-03-06 10:49:23 +0200

    extend application controller with class methods once the paging module is included

--- lib/pagination_headers.rb
+++ lib/pagination_headers.rb
@@ -1,21 +1,24 @@
 module PaginationHeaders
-	protected
-	def self.set_pagination_headers(name, options = {})
-		after_filter(options) do |controller|
-			results = instance_variable_get("@#{name}")
-			response.headers["X-Pagination"] = {
-				total: results.total_entries,
-				total_pages: results.total_pages,
-				first_page: results.current_page == 1,
-				last_page: results.next_page.blank?,
-				previous_page: results.previous_page,
-				next_page:total results.next_page,
-				out_of_bounds: results.out_of_bounds?,
-				offset: results.offset
-			}.to_json
+	module ClassMethods
+		def set_pagination_headers(name, options = {})
+			after_filter(options) do |controller|
+				results = instance_variable_get("@#{name}")
+				response.headers["X-Pagination"] = {
+					total: results.total_entries,
+					total_pages: results.total_pages,
+					first_page: results.current_page == 1,
+					last_page: results.next_page.blank?,
+					previous_page: results.previous_page,
+					next_page: results.next_page,
+					out_of_bounds: results.out_of_bounds?,
+					offset: results.offset
+				}.to_json
+			end
 		end
 	end
 
+	def self.included(base)
+		base.extend ClassMethods
+	end
 end
 
-ApplicationController.send(:include, PaginationHeaders)
